026ef3749185f559c257ff35e5c85b9ce2fc2819
vishalhirani978 <vishalhirani2007@gmail.com>
2026-06-08 02:50:56 +0500

    Initial commit - Car Price Predictor Flask app

---
+++ application.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+
+car = pd.read_csv('cleanedCar.csv')
+model = pickle.load(open('LinearRegressionModel.pkl', 'rb'))
+
+@app.route('/')
+def index():
+    companies = sorted(car['company'].unique())
+    car_models = sorted(car['name'].unique())
+    year = sorted(car['year'].unique(), reverse=True)
+    fuel_types = sorted(car['fuel_type'].unique())
+    return render_template('index.html', companies=companies, car_models=car_models, years=year, fuel_types=fuel_types)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    company = request.form.get('company')
+    car_model = request.form.get('car_model')
+    year = int(request.form.get('year'))
+    fuel_type = request.form.get('fuel_type')
+    kms_driven = int(request.form.get('kms_driven'))
+
+    prediction = model.predict(pd.DataFrame([[car_model, company, year, kms_driven, fuel_type]],
+                                            columns=['name', 'company', 'year', 'kms_driven', 'fuel_type']))
+    return str(round(prediction[0], 2))
+
+if __name__ == '__main__':
+    app.run(debug=True)
